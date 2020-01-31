0586704e149ddb74ef90fc7b2edee3e651fb960b
Lucián Blažek <blazek@1webit.cz>
2020-01-31 23:27:11 +0100

    player movement, shooting started, player entity

---
+++ Assets/Scripts/Player/PlayerEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEntity : MonoBehaviour
+{
+    public static PlayerEntity instance;
+
+    public float playerHeight = 2;
+    public float playerRadius = 0.5f;
+    public float acceleration = 700;
+    public float maxSpeed = 5;
+    public float sprintModifier = 1.3f;
+    public float flyingAccelerationModifier = 0.9f;
+    public float rotationSpeed = 5;
+    public float jumpForce = 4;
+    
+    private void Start()
+    {
+        instance = this;
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -14,12 +14,12 @@ public class PlayerMovement : MonoBehaviour
 
     public float playerHeight = 2;
     public float playerRadius = 0.5f;
-    public float acceleration = 10;
-    public float maxSpeed = 10;
-    public float sprintModifier = 1.5f;
-    public float flyingAccelerationModifier = 0.6f;
+    public float acceleration = 700;
+    public float maxSpeed = 5;
+    public float sprintModifier = 1.3f;
+    public float flyingAccelerationModifier = 0.9f;
     public float rotationSpeed = 5;
-    public float jumpForce = 5;
+    public float jumpForce = 4;
 
     private float headRot = 0;
     
