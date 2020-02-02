5e379df6bc503da735d46712c442521b25386984
Lucián Blažek <blazek@1webit.cz>
2020-02-02 13:25:49 +0100

    idk

--- Assets/Scripts/GameManager/GameSettings.cs
+++ Assets/Scripts/GameManager/GameSettings.cs
@@ -15,14 +15,23 @@ public class GameSettings : MonoBehaviour
 
     public Transform respawnPoint;
     public GameObject Player;
-    
+
+    public AudioClip musicLoop;
+    private AudioSource audioS;
+    private bool finishedIntro = false;
     void Awake()
     {
         instance = this;
+        audioS = GetComponent<AudioSource>();
     }
 
     void Update()
     {
+        if (!finishedIntro && !audioS.isPlaying)
+        {
+            audioS.loop = true;
+            audioS.PlayOneShot(musicLoop);
+        }
         if (Input.GetKeyDown(KeyCode.Return))
         {
             Player.transform.position = respawnPoint.position;
